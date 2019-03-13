11f9b7400dd79fccd0d0ec29c242711db0b9ed3e
Rafal.Krakiewicz <Rafal.Krakiewicz@WAW-JT9NV02.corpnet.liox.org>
2019-03-13 09:55:34 +0100

    a

--- DTAC/src/main/java/DTAC/Helper/Dtac.java
+++ DTAC/src/main/java/DTAC/Helper/Dtac.java
@@ -6,7 +6,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -37,10 +41,22 @@ public class Dtac {
 
 	public void repair(StyledDocument doc, JButton btnDtac) throws IOException, ZipException, BadLocationException {
 
+		String logMessage = "";
+		PrintWriter writer = new PrintWriter("dtac.log", "UTF-8");
+
+		logMessage = "trying to get export folder\n";
+		log(logMessage);
+
 		String exportFolder = (getExportFolder());
 
+		logMessage = "the patch is: " + exportFolder + "\n"; 	
+		log(logMessage);
+		
 		File cruFiles = new File(exportFolder);
 
+		logMessage = "got cru files\n"; 	
+		log(logMessage);
+		
 		FilenameFilter filter = new FilenameFilter() {
 
 			@Override
@@ -51,10 +67,24 @@ public class Dtac {
 
 		String[] cruFilesNames = cruFiles.list(filter);
 
+		logMessage = "got cru names\n"; 	
+		log(logMessage);
+		
 		checkCwuFiles(exportFolder, cruFilesNames, doc, btnDtac);
-
+		logMessage = "did check\n"; 	
+		log(logMessage);
+		writer.close();
 	}
 
+	private void log(String message) throws IOException {
+		try {
+			Files.write(Paths.get("dtac.log"), message.getBytes(), StandardOpenOption.APPEND);
+		} catch (Exception e) {
+			String tmp = e.getMessage();
+			Files.write(Paths.get("dtac.log"), tmp.getBytes(), StandardOpenOption.APPEND);
+		}
+	}
+	
 	private static void checkCwuFiles(String exportFolder, String[] cruFilesNames, StyledDocument doc, JButton btnDtac)
 			throws ZipException, IOException, BadLocationException {
 
@@ -62,14 +92,16 @@ public class Dtac {
 		Style style = jtp.addStyle("Red coloured text", null);
 		StyleConstants.setForeground(style, Color.green);
 		StyleConstants.setAlignment(style, 1);
+		doc.insertString(doc.getLength(), "Start\n", null);
 		
 		btnDtac.setEnabled(false);
 		for (String fileName : cruFilesNames) {
 
-			String databasePatch = unzipCruFile(new File(exportFolder + fileName));
+			doc.insertString(doc.getLength(), "found: " + cruFilesNames.length + " files\n", null);
+			String databasePatch = unzipCruFile(new File(exportFolder + "\\" + fileName));
 			doc.insertString(doc.getLength(), "\n" + fileName, null);
 
-			boolean check = checkDatabase(databasePatch);
+			boolean check = checkDatabase(databasePatch,doc);
 			// System.out.println(check);
 
 			if (!check) {
@@ -83,17 +115,18 @@ public class Dtac {
 			FileUtils.deleteDirectory(new File(databasePatch));
 		}
 		doc.insertString(doc.getLength(), "\nFINISHED", null);
+		Files.write(Paths.get("the-file-name.txt"), "koniec\\n".getBytes(), StandardOpenOption.APPEND);
 		btnDtac.setEnabled(true);
 	}
 
 	private static void repairDatabase(String exportFolder, String databasePatch) throws ZipException, IOException {
-		
+
 		String newCwuFileName = zipFile(databasePatch);
 
 		changeExtension(new File(newCwuFileName), "cwu");
 
 		FileUtils.copyFileToDirectory(new File(newCwuFileName.replace(".zip", ".cwu")), new File(exportFolder));
-		
+
 	}
 
 	public static String zipFile(String databasePatch) throws ZipException {
@@ -145,21 +178,40 @@ public class Dtac {
 		return tmp.replace("\r\n", "");
 	}
 
-	public static boolean checkDatabase(String databasePatch) throws IOException {
+	public static boolean checkDatabase(String databasePatch, StyledDocument doc) throws IOException, BadLocationException {
 
 		// true - baza nie zawiera bledow
 		// false - baza z bledami, naprawiono
 
 		boolean result = true;
-
+		doc.insertString(doc.getLength(), "opening database\n", null);
 		Database db = DatabaseBuilder.open(new File(databasePatch + "\\unit.mdb"));
+		doc.insertString(doc.getLength(), "database opened\n", null);
 		Table table = db.getTable("Projects");
-
+		doc.insertString(doc.getLength(), "table opened\n", null);
+		int i = 0;
 		for (Row row : table) {
+			doc.insertString(doc.getLength(), "checking row: " + i + "\n" , null);	
 			if (row.containsValue("Mariusz Ziolkowski test prep")) {
-				table.deleteRow(row);
+				doc.insertString(doc.getLength(), "got error\n" , null);
+				try {
+					doc.insertString(doc.getLength(), row.toString()  + "\n" , null);
+					table.deleteRow(row);	
+				} catch (Exception e) {
+					doc.insertString(doc.getLength(), "error: " + e.getMessage() + "\n", null);
+				}
+				doc.insertString(doc.getLength(), "id wiersza: " + row.getId() + "\n", null);
+				
+				try {
+					doc.insertString(doc.getLength(), row.toString()  + "\n" , null);
+					table.deleteRow(row);	
+				} catch (Exception e) {
+					doc.insertString(doc.getLength(), "error2: " + e.getMessage() + "\n", null);
+				}
+				doc.insertString(doc.getLength(), "error fixed\n" , null);
 				result = false;
 			}
+			i++;
 		}
 
 		db.close();
--- DTAC/src/main/java/DTAC/Helper/Layout.java
+++ DTAC/src/main/java/DTAC/Helper/Layout.java
@@ -10,7 +10,16 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -29,8 +38,12 @@ public class Layout {
 
 	/**
 	 * Launch the application.
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		PrintWriter writer = new PrintWriter("main.log", "UTF-8");
+		Files.write(Paths.get("main.log"), "START\n".getBytes(), StandardOpenOption.APPEND);				
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -41,6 +54,8 @@ public class Layout {
 				}
 			}
 		});
+		Files.write(Paths.get("main.log"), "STOP\n".getBytes(), StandardOpenOption.APPEND);
+		writer.close();
 	}
 
 	/**
@@ -60,12 +75,31 @@ public class Layout {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JButton btnDtac = new JButton("FIX");
 		JTextPane txtpnDs = new JTextPane();
+		JTextPane txtpnStart = new JTextPane();
+		txtpnStart.setText("ready");
 		txtpnDs.setEditable(false);
 		txtpnDs.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		txtpnDs.setBackground(UIManager.getColor("Button.background"));
 		StyledDocument doc = txtpnDs.getStyledDocument();
 		btnDtac.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				
+				Runnable counter = () -> {
+					try {
+						for (int i = 0; i < 100; i++) {
+							TimeUnit.SECONDS.sleep(1);
+							String timeStamp = new SimpleDateFormat("mmss").format(Calendar.getInstance().getTime());
+							txtpnStart.setText(timeStamp);	
+						}
+						
+					} catch (Exception e2) {
+						txtpnStart.setText(e2.getMessage());
+					}									
+				};
+				Thread thread2 = new Thread(counter);
+				thread2.start();
+				
 				Dtac dtac = new Dtac();
 					
 				Runnable task = () -> {
@@ -86,20 +120,23 @@ public class Layout {
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(18)
-							.addComponent(txtpnDs, GroupLayout.PREFERRED_SIZE, 556, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(277)
-							.addComponent(btnDtac)))
+					.addGap(18)
+					.addComponent(txtpnDs, GroupLayout.PREFERRED_SIZE, 556, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(46, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(277)
+					.addComponent(btnDtac)
+					.addPreferredGap(ComponentPlacement.RELATED, 173, Short.MAX_VALUE)
+					.addComponent(txtpnStart, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+					.addGap(68))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(57)
-					.addComponent(btnDtac)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnDtac)
+						.addComponent(txtpnStart, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE))
 					.addGap(41)
 					.addComponent(txtpnDs, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(30, Short.MAX_VALUE))
